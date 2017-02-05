88f28d8b0302f5b023a14ea6d682150699e48cf6
sosnovska <gosia_s1@o2.pl>
2017-02-05 15:17:58 +0000

    Prototype of the webpage for a language school in Polish

---
+++ kontakt.php
@@ -0,0 +1,46 @@
+﻿<?php
+
+// Tworzymy zmienną dla imienia i nazwiska
+$name = $_POST['name'];
+
+// Tworzymy zmienną dla adresu email
+$email = $_POST['email'];
+
+// Tworzymy zmienną dla telefonu
+$tel = $_POST['tel'];
+
+// Tworzymy zmienną dla wiadomości
+$message = $_POST['message'];
+
+// Podajesz adres email z którego ma być wysłana wiadomość
+$odkogo = ".@.";
+
+// Podajesz adres email na który chcesz otrzymać wiadomość
+$dokogo = "msosnowska0@gmail.com";
+
+// Podajesz tytuł jaki ma mieć ta wiadomość email
+$tytul = "Formularz kontaktowy dialektika";
+
+// Przygotowujesz treść wiadomości
+$wiadomosc = "";
+$wiadomosc .= "Imię i nazwisko: " . $name . "\n";
+$wiadomosc .= "Email: " . $email . "\n";
+$wiadomosc .= "Telefon: " . $tel . "\n";
+$wiadomosc .= "Wiadomość: " . $message . "\n";
+
+// Dodajemy UTF-8 do naglowka naszej wiadomości
+$naglowek = "";
+$naglowek .= "Od:" . $odkogo . " \n";
+$naglowek .= "Content-Type:text/plain;charset=utf-8";
+
+// Wysyłamy wiadomość
+$sukces = mail($dokogo, $tytul, $wiadomosc, "Od: <$odkogo>");
+
+// Przekierowywujemy na potwierdzenie
+if ($sukces){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=potwierdzenie.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+}
+?>
